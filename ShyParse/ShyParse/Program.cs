cb7454b0d9c1f3a8b1b9accc0d53b6d5a03fd7c1
dudushy <eduardotalarico1337@gmail.com>
2022-07-26 19:53:02 -0300

    Some progress

--- ShyParse/ShyParse/Program.cs
+++ ShyParse/ShyParse/Program.cs
@@ -1,12 +1,24 @@
 ﻿//* Header
 Console.WriteLine("\t=-- ShyParse.cs --=");
-Console.WriteLine("- Attempting to recreate TryParse(). -");
+Console.WriteLine("- Attempting to recreate TryParse() -");
+Console.WriteLine("-                                   -");
 
 //* CODE [BEGIN]
-Console.Write("- input: ");
-string input = Console.ReadLine();
-Console.WriteLine("- input = " + input);
+int ShyParse(string input)
+{
+    Console.WriteLine("-                                   -");
+
+    Console.WriteLine("- ShyParse(" + input + ")");
+
+    Console.WriteLine("-                                   -");
+    return 0;
+}
+
+Console.Write("- rawString: ");
+string rawString = Console.ReadLine();
+
+ShyParse(rawString);
 //* CODE [END]
 
 //* Footer
-Console.WriteLine("- - - - - - - - - - - - - - - - - - - -");
+Console.WriteLine("- - - - - - - - - - - - - - - - - - -");
