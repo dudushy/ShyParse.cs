c98997787d71146724067bc5526feefc7cc021eb
dudushy <eduardotalarico1337@gmail.com>
2022-07-26 20:01:15 -0300

    Finally finished the setup

---
+++ ShyParse/Program.cs
@@ -0,0 +1,24 @@
+﻿//* Header
+Console.WriteLine("\t=-- ShyParse.cs --=");
+Console.WriteLine("- Attempting to recreate TryParse() -");
+Console.WriteLine("-                                   -");
+
+//* CODE [BEGIN]
+int ShyParse(string input)
+{
+    Console.WriteLine("-                                   -");
+
+    Console.WriteLine("- ShyParse(" + input + ")");
+
+    Console.WriteLine("-                                   -");
+    return 0;
+}
+
+Console.Write("- rawString: ");
+string rawString = Console.ReadLine();
+
+ShyParse(rawString);
+//* CODE [END]
+
+//* Footer
+Console.WriteLine("- - - - - - - - - - - - - - - - - - -");
